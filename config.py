cf8ebbcf8acfcdb33f363ce279e5a7ae992f871a
Hernán <hcaglia@gmail.com>
2021-01-18 03:16:08 -0300

    questions and user info added

--- config.py
+++ config.py
@@ -1,8 +1,64 @@
-PRODUCTS = ['Motorola G6', 'Chromecast', 'Notebook', 'Mac', 'Macbook']
+products = {
+    'Motorola G6',
+    'Chromecast',
+    'Notebook',
+    'Samsung S20',
+    'Macbook'
+}
 
-API_SEARCH = 'https://api.mercadolibre.com/sites/MLA/search?q='
-SEARCH_LIMIT = 10
+api_search = dict(
+    url='https://api.mercadolibre.com/sites/MLA/search?q=',
+    search_limit=10
+)
 
-API_ITEM = 'https://api.mercadolibre.com/items/'
-API_BLABLA = ''
-API_BLABLA2 = ''
+api_item = dict(
+    url='https://api.mercadolibre.com/items/',
+    variables={
+        'id',
+        'title',
+        'seller_id',
+        'category_id',
+        'official_store_id',
+        'price', 'base_price',
+        'original_price',
+        'initial_quantity',
+        'available_quantity',
+        'sold_quantity',
+        'buying_mode',
+        'listing_type_id',
+        'start_time',
+        'condition',
+        'accepts_mercadopago',
+        'seller_address.country.name',
+        'seller_address.state.name',
+        'status',
+        'warranty',
+        'tags',
+        'catalog_product_id',
+        'domain_id',
+        'health'
+    }
+)
+
+api_questions = dict(
+    url='https://api.mercadolibre.com/questions/search?item=',
+    variables={
+        'total'
+    }
+)
+
+api_users = dict(
+    url='https://api.mercadolibre.com/users/',
+    variables={
+        'nickname',
+        'user_type',
+        'seller_reputation.level_id',
+        'seller_reputation.power_seller_status',
+        'seller_reputation.transactions.canceled',
+        'seller_reputation.transactions.completed',
+        'seller_reputation.transactions.ratings.positive',
+        'seller_reputation.transactions.ratings.neutral',
+        'seller_reputation.transactions.ratings.negative'
+        ''
+    }
+)
--- main.py
+++ main.py
@@ -2,6 +2,16 @@ import requests
 import pandas as pd
 import config
 
+"""
+Mejoras:
+Agregar las variables deseadas en los param del endpoint para traer solo esas. Luego se puede sacar el filtro de pd.
+Multiget todos los items a la vez? Necesita token.
+Hacer requirements.txt
+Mover servicios a meli_service
+Check blank lines add
+try catch en item info?
+"""
+
 OFFSET_PARAM = '&offset='
 LIMIT_PARAM = '&limit='
 LIMIT_VALUE = '50'
@@ -13,7 +23,7 @@ def create_search_string(search):
 
 
 def get_item_ids(search, offset):
-    response = requests.get(config.API_SEARCH + search + LIMIT_PARAM + LIMIT_VALUE + OFFSET_PARAM + str(offset)).json()
+    response = requests.get(config.api_search['url'] + search + LIMIT_PARAM + LIMIT_VALUE + OFFSET_PARAM + str(offset)).json()
     try:
         response_data = pd.json_normalize(response, record_path='results')
         item_ids = response_data['id']
@@ -25,62 +35,95 @@ def get_item_ids(search, offset):
 
 def build_items():
     items_df = pd.DataFrame(columns=['product', 'item_id'])
-    for product in config.PRODUCTS:
+    for product in config.products:
+        print('Getting items from ' + product + '...')
         product_string = create_search_string(product)
         offset = 0
         item_count = 0
         product_df = pd.DataFrame(columns=['product', 'item_id'])  # Create DF from this product's items
-        while item_count < config.SEARCH_LIMIT:
+        while item_count < config.api_search['search_limit']:
             items_id = get_item_ids(search=product_string, offset=offset)
             product_df = product_df.append(pd.DataFrame({'product': product, 'item_id': items_id}), ignore_index=True)
             offset += 50
             item_count += len(items_id)
-        items_df = items_df.append(product_df[0:config.SEARCH_LIMIT], ignore_index=True)  # Add items to DF
+        items_df = items_df.append(product_df[0:config.api_search['search_limit']], ignore_index=True)  # Add items to DF
     items_df.set_index('item_id', inplace=True)
     return items_df
 
 
 def get_item_info(items):
+    print('Getting items info...')
     items_info_df = pd.DataFrame()
     for item in items.iterrows():
         item_id = item[0]
-        response = requests.get(config.API_ITEM + item_id).json()
-        item_info = pd.json_normalize(response, meta=['seller_address', 'country', 'name'])
-        item_info = item_info[['id',
-                               'seller_id',
-                               'category_id',
-                               'official_store_id',
-                               'price', 'base_price',
-                               'original_price',
-                               'initial_quantity',
-                               'available_quantity',
-                               'sold_quantity',
-                               'buying_mode',
-                               'listing_type_id',
-                               'start_time',
-                               'condition',
-                               'accepts_mercadopago',
-                               'seller_address.country.name',
-                               'seller_address.state.name',
-                               'status',
-                               'tags',
-                               'warranty',
-                               'catalog_product_id',
-                               'domain_id',
-                               'health'
-                               ]].set_index('id')
+        response = requests.get(config.api_item['url'] + item_id).json()
+        item_info = pd.json_normalize(response)
+        item_info = item_info[config.api_item['variables']].set_index('id')
         item_info['item_id'] = item_id
         items_info_df = items_info_df.append(item_info)
     return items_info_df
 
 
+def get_item_questions(items):
+    print('Getting questions info...')
+    item_questions = pd.DataFrame()
+    for item in items.iterrows():
+        item_id = item[0]
+        response = requests.get(config.api_questions['url'] + item_id).json()
+        response_df = pd.json_normalize(response)
+        questions_info = pd.DataFrame()
+        questions_info['item_id'] = item_id
+        questions_info.set_index('item_id', inplace=True)
+        for variable in config.api_questions['variables']:
+            variable_name = 'questions_' + variable
+            try:
+                questions_info.loc[item_id, variable_name] = response_df[variable][0]
+            except:
+                questions_info.loc[item_id, variable_name] = ''
+        item_questions = item_questions.append(questions_info)
+    return item_questions
+
+
+def get_user_info(user_ids):
+    print('Getting users info...')
+    user_info = pd.DataFrame()
+    for user in user_ids:
+        response = requests.get(config.api_users['url'] + str(user)).json()
+        response_df = pd.json_normalize(response)
+        user_df = pd.DataFrame()
+        user_df['user_id'] = user
+        user_df.set_index('user_id', inplace=True)
+        for variable in config.api_users['variables']:
+            variable_name = 'users_' + variable
+            try:
+                user_df.loc[user, variable_name] = response_df[variable][0]
+            except:
+                user_df.loc[user, variable_name] = ''
+        user_info = user_info.append(user_df)
+    return user_info
+
+
 def main():
     items = build_items()
+    items.to_csv('1_items.csv', sep=',')
+
     items_info = get_item_info(items)
+    items_info.to_csv('2_items_info.csv', sep=',')
+
     result = items.merge(items_info, how='left', left_on='item_id', right_on='id', suffixes=[None, '_i'])
-    result.to_csv('result.csv', sep=',')
+    result.to_csv('3_result.csv', sep=',')
 
+    item_questions = get_item_questions(items)
+    item_questions.to_csv('4_item_questions.csv', sep=',')
 
+    result = result.merge(item_questions, how='left', left_on='item_id', right_on='item_id', suffixes=[None, '_q'])
+    result.to_csv('5_result.csv', sep=',')
+
+    user_info = get_user_info(pd.unique(items_info['seller_id']))
+    user_info.to_csv('6_user_info.csv', sep=',')
+
+    result = result.reset_index().merge(user_info, how='left', left_on='seller_id', right_on='user_id', suffixes=[None, '_u'])
+    result.to_csv('7_result.csv', sep=',')
 
 if __name__ == '__main__':
     main()
